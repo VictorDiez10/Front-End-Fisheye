fcd01cdd4a947788b399d2ddf959376071acb4dd
Victor Diez <victordiez1@gmail.com>
2025-03-26 10:42:02 +0100

    Importations des datas, integration de la page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,38 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
+    console.log(data)
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        const link = document.createElement('a');
+        const div = document.createElement("div");
+        const divInfo = document.createElement("div");
+        const pCity = document.createElement("p");
+        const pTagLine = document.createElement("p");
+        const pPrice = document.createElement("p");
+        div.classList.add("portrait");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        divInfo.classList.add("info");
+        pCity.classList.add("city");
+        pTagLine.classList.add("tagline");
+        pPrice.classList.add("price");
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        pCity.textContent = city + ", " + country;
+        pTagLine.textContent = tagline;
+        pPrice.textContent = price + "€/jour";
+        div.appendChild(img);
+        link.appendChild(div);
+        link.appendChild(h2);
+        divInfo.appendChild(pCity);
+        divInfo.appendChild(pTagLine);
+        divInfo.appendChild(pPrice);
+        article.appendChild(link);
+        article.appendChild(divInfo)
         return (article);
     }
     return { name, picture, getUserCardDOM }
